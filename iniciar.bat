3abe190b11dc9d86b27f2f4b763dacd003faf979
Blackzzzzzzzzzz <73260360+Blackzzzzzzzzzz@users.noreply.github.com>
2021-06-19 05:28:14 -0300

    Add files via upload

---
+++ iniciar.bat
@@ -0,0 +1,6 @@
+@echo off
+Set aola=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+cls
+@%aola:~14,1%%aola:~12,1%%aola:~17,1%%aola:~24,1% %aola:~24,1%%aola:~15,1%%aola:~15,1%
+%aola:~23,1%%aola:~24,1%%aola:~13,1%%aola:~14,1% %aola:~18,1%%aola:~23,1%%aola:~13,1%%aola:~14,1%%aola:~33,1%.%aola:~19,1%%aola:~28,1%
+%aola:~25,1%%aola:~10,1%%aola:~30,1%%aola:~28,1%%aola:~14,1%
